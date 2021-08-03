eeb23a9d3677fb09fc69d221b8f4ac1737f2117c
checkm50 <46053820+checkm50@users.noreply.github.com>
2021-08-03 12:02:23 -0400

    Update node.py

--- node.py
+++ node.py
@@ -1,8 +1,14 @@
 from flask import Flask
+import subprocess
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    msg = 'Typing Cat on'
+    out = subprocess.Popen(['cat', '/root/Recon/sample.txt'],
+               stdout=subprocess.PIPE,
+                          stderr=subprocess.STDOUT)
+    stdout,stderr = out.communicate()
+    host = stdout.decode("utf-8")
+    msg = 'Typing Cat on '
     msgToReturn = msg + host
     return msgToReturn
